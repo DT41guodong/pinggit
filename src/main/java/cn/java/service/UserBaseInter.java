407a4a83be745d4647f90b2aac445300ffb09a97
DT41guodong <13627265857@163.com>
2018-12-23 17:14:00 +0800

    123321123

--- src/main/java/cn/java/service/UserBaseInter.java
+++ src/main/java/cn/java/service/UserBaseInter.java
@@ -1,5 +1,11 @@
 package cn.java.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface UserBaseInter {
 
+	void insert(Map<String,Object> map);
+	
+	List<Map<String,Object>> getList();
 }
--- src/main/java/cn/java/service/impl/UserBaseImpl.java
+++ src/main/java/cn/java/service/impl/UserBaseImpl.java
@@ -1,11 +1,26 @@
 package cn.java.service.impl;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import cn.java.service.UserBaseInter;
-
+/**
+ * @author Administrator
+ *
+ */
 public class UserBaseImpl implements UserBaseInter {
 
-  public final static Map<String,Object> reqMap=new HashMap<>();
+	public final static Map<String, Object> reqMap = new HashMap<>();
+
+	@Override
+	public void insert(Map<String, Object> map) {
+
+	}
+
+	@Override
+	public List<Map<String, Object>> getList() {
+
+		return null;
+	}
 }
