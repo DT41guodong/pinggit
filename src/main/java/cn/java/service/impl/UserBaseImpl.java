06681d57c0e12df6cc69873560f029d7a9fb4424
DT41guodong <13627265857@163.com>
2018-12-23 16:40:23 +0800

    13123

--- src/main/java/cn/java/service/impl/UserBaseImpl.java
+++ src/main/java/cn/java/service/impl/UserBaseImpl.java
@@ -1,7 +1,11 @@
 package cn.java.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import cn.java.service.UserBaseInter;
 
 public class UserBaseImpl implements UserBaseInter {
 
+  public final static Map<String,Object> reqMap=new HashMap<>();
 }
